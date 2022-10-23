c254c0436273bfaa681fcfe5c21068794750e751
Andrew <andreybybnov435@yandex.ru>
2022-10-23 21:04:33 +0500

    2

---
+++ 1_Part/2_Ошибки/2_Задачи/Mistakes/Task_3_DoSomethingDontKnowWhat/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task_3_DoSomethingDontKnowWhat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Decode("12...33.44");
+        }
+
+        static int Decode(string str) =>
+            (int.Parse(str.Replace(".", "")) % 1024);
+    }
+}
